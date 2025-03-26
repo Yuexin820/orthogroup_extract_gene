8bdb8e64635595430edc72cde1a8ce8ed9e48543
Yuexin820 <yuexin_wang@snnu.edu.cn>
2025-03-26 11:38:03 +0800

    Add files via upload

---
+++ orthogroup_extract_gene.pl
@@ -0,0 +1,44 @@
+## .pl <Heterotrichea_single_contract.significant> <Orthogroups.tsv>
+use strict;
+
+my ($geneFamily,$genes) = @ARGV;
+
+my %geneFams;
+open FILE,"$geneFamily" or die $!;
+while (<FILE>)
+{
+	chomp;
+	$geneFams{$_} = "y";
+}
+close FILE;
+
+my @species;
+open FILE,"$genes" or die $!;
+while (my $line = <FILE>)
+{
+	chomp($line);
+	my @line_words = split /\t/,$line;
+	if($line_words[0] eq "Orthogroup")
+	{
+		for(my $i=1;$i<@line_words;$i++)
+		{
+			open my $fh,">Ortho_"."$line_words[$i]" or die $!;
+			$species[$i] = $fh;
+		}
+		next;
+	}
+	elsif($geneFams{$line_words[0]} eq 'y')
+	{
+		for(my $i=1;$i<@line_words;$i++)
+		{
+			my @ids = split /,/,$line_words[$i];
+			map{
+				s/ //g;
+				if(/\w/){print {$species[$i]} "$_\n"}
+			}@ids;
+		}
+	}
+}
+close FILE;
+
+map{close $_}@species;
